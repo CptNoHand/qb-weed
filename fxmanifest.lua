27534e8bade4760d39152c07d8d06675a8602541
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2022-01-22 16:31:26 +0100

    refactor 3.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,11 +6,22 @@ version '1.0.0'
 
 shared_script 'config.lua'
 
-client_script 'client/main.lua'
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'client/main.lua', 
+    'client/drying.lua', 
+    'client/processing.lua', 
+}
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+    'server/main.lua',
+    'server/processing.lua',
+    'server/drying.lua',
 }
 
 lua54 'yes'
